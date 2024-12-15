ea2fadc68e144a474b061c364b3b3a7ba667ac95
mad-jix <jackjaseelk@gmail.com>
2024-12-15 22:25:56 +0530

    IsDriver

---
+++ driver/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission
+
+class IsDriver(BasePermission):
+    """
+    Allows access only to authenticated staff users.
+    """
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated and request.user.is_staff)
