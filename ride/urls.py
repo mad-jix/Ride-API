99c8a65ec1ee0b5539cab6178ff28084fcc4874c
mad-jix <jackjaseelk@gmail.com>
2024-12-17 23:08:56 +0530

    ride urls

--- ride/urls.py
+++ ride/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import RideCreateView, RideDetail, ListRides,UpdateRideStatusView,RideRequestCreateView,RideRequestAcceptView,ListRidesRequest
+from .views import RideCreateView, RideDetail, ListRides,UpdateRideStatusView,RideRequestCreateView,ListRidesRequest
 urlpatterns = [
-    path('ridecreate/', RideCreateView.as_view(), name='ride-create'),
-    path('rides/<int:pk>/', RideDetail.as_view(), name='ride_detail'),
-    path('rides/all/', ListRides.as_view(), name='list_rides'),
-    path('api/rides/<int:pk>/status/', UpdateRideStatusView.as_view(), name='update_ride_status'),
-    path('ride-request/create/', RideRequestCreateView.as_view(), name='ride-request-create'),
-    path('ride-request/<int:pk>/accept/', RideRequestAcceptView.as_view(), name='accept the rice'),
-    path('allrequest/', ListRidesRequest.as_view(), name='list_rides-request'),
+    path('ride-create/', RideCreateView.as_view(), name='ride-create'),
+    path('detial-view/<int:pk>/', RideDetail.as_view(), name='ride_detail'),
+    path('all-rides/', ListRides.as_view(), name='list_rides'),
+    path('status-update/<int:pk>/', UpdateRideStatusView.as_view(), name='update_ride_status'),
+    path('ride-request-create/', RideRequestCreateView.as_view(), name='ride-request-create'),
+    path('all-request/', ListRidesRequest.as_view(), name='list_rides-request'),
     
 ]
