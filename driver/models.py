0f166a49d248d8c3aee41fd2a955df84bb61577b
mad-jix <jackjaseelk@gmail.com>
2024-12-17 22:55:49 +0530

    driver models

--- driver/models.py
+++ driver/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Driver(models.Model):
-    driver = models.OneToOneField(User, related_name='driver', on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='driver', on_delete=models.SET_NULL, null=True, blank=True)
     latitude = models.FloatField(default=0.0)
     longitude = models.FloatField(default=0.0)
     is_available = models.BooleanField(default=True)
