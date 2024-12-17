ffa1aef5882beef9d05a1630d04a538f44166018
mad-jix <jackjaseelk@gmail.com>
2024-12-17 22:56:33 +0530

    driver serializers

--- driver/serializers.py
+++ driver/serializers.py
@@ -5,12 +5,12 @@ from .models import Driver
 class DriverSerializer(serializers.ModelSerializer):
     class Meta:
         model = Driver
-        fields = ['id', 'driver', 'latitude', 'longitude', 'is_available']
+        fields = ['id', 'user', 'latitude', 'longitude', 'is_available']
 
     def create(self, validated_data):
-        driver = validated_data.get('driver')
+        user = validated_data.get('user')
         
-        if not driver.is_staff:
+        if not user.is_staff:
             raise serializers.ValidationError("Only staff users can create a driver.")
         
         return Driver.objects.create(**validated_data)
