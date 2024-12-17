c9a040b21849965e052cf58d856b41d16997a966
mad-jix <jackjaseelk@gmail.com>
2024-12-17 22:59:00 +0530

    user models

--- user/models.py
+++ user/models.py
@@ -1,27 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('rider', 'Rider'),
+        ('driver', 'Driver'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='rider')
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(username, email, password, **extra_fields)
+    def save(self, *args, **kwargs):
+        if not self.is_superuser:
+            if self.role == 'driver':
+                self.is_staff = True
+            else:
+                self.is_staff = False
+        super().save(*args, **kwargs)
 
-class CustomUser(AbstractBaseUser):
-    username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(unique=True)
-    # other fields
+    def is_driver(self):
+        return self.role == 'driver'
 
-    objects = CustomUserManager()
-    
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    def is_rider(self):
+        return self.role == 'rider'
