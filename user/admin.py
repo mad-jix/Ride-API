0145823f3425452978c13a196dad7a89c199911a
mad-jix <jackjaseelk@gmail.com>
2024-12-17 22:58:44 +0530

    admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}), 
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role',)}),  
+    )
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')  
+    list_filter = ('role', 'is_staff', 'is_active')  
+    search_fields = ('username', 'email') 
