5a483c1af507f95c505000222ed9abfaadd6f19e
mad-jix <jackjaseelk@gmail.com>
2024-12-25 09:36:03 +0530

    ride views

--- ride/views.py
+++ ride/views.py
@@ -1,34 +1,32 @@
 
 from math import radians, cos, sin, sqrt, atan2
 
-from django.core.exceptions import ObjectDoesNotExist
-
 from rest_framework import generics, permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
-from rest_framework.exceptions import PermissionDenied
 
-from driver.permissions import IsDriver
 from driver.models import Driver
+
 from .models import Ride, RideRequest
 from .serializers import RideSerializer,RideRequestSerializer, RideStatusUpdateSerializer
-
 
 class RideCreateView(generics.CreateAPIView):
     queryset = Ride.objects.all()
     serializer_class = RideSerializer
-    permission_classes = [permissions.AllowAny]
 
-    def perform_create(self, serializer):
-        serializer.save(rider=self.request.user)
-
+    def get_serializer_context(self):
+        return {'request': self.request}
 
     def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        return Response({"message": "Ride created successfully", "ride": response.data}, status=status.HTTP_201_CREATED)
-
+        response = super().create(request, *args, **kwargs) 
+        return Response(
+            {
+                "message": "Ride created successfully",
+                "ride": response.data,
+            },
+            status=status.HTTP_201_CREATED,
+        )
 
 class RideDetail(generics.RetrieveAPIView):
     queryset = Ride.objects.all()
