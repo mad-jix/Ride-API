ad1b975a9d8d59417f60ac7cc0dfbbffdc7eeac2
mad-jix <jackjaseelk@gmail.com>
2024-12-25 09:37:03 +0530

    drivere urls

--- driver/urls.py
+++ driver/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import DriverCreateView,DriverListView,DriverUpdateView,RideRequestAcceptView
+from .views import DriverCreateView,DriverListView,UpdateDriverView,RideRequestAcceptView
 
 urlpatterns = [
 
     path('create', DriverCreateView.as_view(), name='driver-create'),
     path('list/', DriverListView.as_view(), name='all-driverse'),
-    path('change/<int:pk>/', DriverUpdateView.as_view(), name='driver-update'),
+    path('update/<int:pk>/', UpdateDriverView.as_view(), name='driver-update'),
     path('ride-accept/<int:pk>/', RideRequestAcceptView.as_view(), name='accept-ride-request'),
 
 
+
 ]
+
+
